889338dff2777e95fc2dc44dedd4e2f7f1a0e634
hugh <hugh_fly@hotmail.com>
2017-05-25 16:58:28 +1200

    backup 25/5 evening

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Post
+#.models means in the current folder
 
 def post_list(request):
     '''Show the list of the blog
     and it is the first view
     '''
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request):
+    post = Post.objects.get()
+    return render(request, 'blog/post_detail.html',{'post':post})
